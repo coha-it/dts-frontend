8a5ac4267191de2f59cd3ad644bd90e1e131ec35
baffi <baffi@baffis-MacBook-Pro.fritz.box>
2021-02-02 20:04:52 +0100

    Export CSV Stats

--- src/components/Backend/StatisticStatistics.vue
+++ src/components/Backend/StatisticStatistics.vue
@@ -3,37 +3,55 @@ div
   template(v-if="getSurveyIds()")
     | Wähle Statstiken für die Umfragen mit der ID: {{ getSurveyIds() }}
     br
-  q-select(
-    filled="",
-    v-model="selectedStatistic",
-    use-input="",
-    input-debounce="0",
-    label="Simple filter",
-    :options="statistics",
-    style="width: 250px",
-    behavior="menu",
-    @filter="filterFn"
-  )
-    // Options
-    template(v-slot:option="scope")
-      q-item(v-bind="scope.itemProps", v-on="scope.itemEvents")
-        q-item-section(avatar)
-          q-icon(:name="scope.opt.icon")
-        q-item-section
-          q-item-label(v-html="scope.opt.label")
-          q-item-label(caption) {{ scope.opt.description }}
-    // Nothing Selected
-    template(v-slot:no-option="")
-      q-item
-        q-item-section.text-grey
-          | No results
-    // Clear Button
-    template(v-slot:append)
-      q-icon.cursor-pointer(
-        v-if="selectedStatistic !== null",
-        name="clear",
-        @click.stop="selectedStatistic = null"
-      )
+
+  .q-gutter-md.row.items-start
+    q-select(
+      filled
+      v-model="selectedStatistic"
+      use-input
+      input-debounce="0",
+      label="Select Statistic",
+      :options="statistics",
+      style="width: 250px",
+      behavior="menu",
+      @filter="filterFn"
+    )
+      // Options
+      template(v-slot:option="scope")
+        q-item(v-bind="scope.itemProps", v-on="scope.itemEvents")
+          q-item-section(avatar)
+            q-icon(:name="scope.opt.icon")
+          q-item-section
+            q-item-label(v-html="scope.opt.label")
+            q-item-label(caption) {{ scope.opt.description }}
+      // Nothing Selected
+      template(v-slot:no-option="")
+        q-item
+          q-item-section.text-grey
+            | No results
+      // Clear Button
+      template(v-slot:append)
+        q-icon.cursor-pointer(
+          v-if="selectedStatistic !== null",
+          name="clear",
+          @click.stop="selectedStatistic = null"
+        )
+    q-input(
+      v-model="filterData.limit"
+      filled
+      use-input
+      type="number"
+      label="Limit"
+      color="orange"
+      placeholder="Kein Limit"
+      style="max-width: 200px"
+      clearable
+      bottom-slots
+      hint="Limitieren Sie die Ausgabe"
+      :error="!!filterData.limit"
+      error-message="Warning: ein Limit beschränkt die Ausgabe der Ergebnisse"
+    )
+
 
   // Load Statitics Button
   br
@@ -49,44 +67,76 @@ div
 
   // If Stats
   template(v-if="stats")
-    // Only Stats THREE
-    template(v-if="currentStatisticId === 3")
-      div jo
 
-    // If not Three
-    template(v-else)
+    q-select(
+      filled
+      v-model="selectedView"
+      use-input
+      input-debounce="0",
+      label="Select View",
+      :options="views",
+      style="width: 250px",
+      behavior="menu"
+    )
+
+    template
       // Go Through Stats
-      div
+      div(v-if="selectedView.id === 1")
         span(v-for="head in stats.header") {{ head }};
         div(v-for="(tr, i) in stats.data")
           span {{ i }}
           span(v-for="td in tr") {{ td }};
 
       // If Stats-Survey
-      template(v-if="stats.surveys")
-        div(v-for="survey in stats.surveys")
-          q-table(
-            :columns="survey.questions",
-            :data="survey.awnsers",
-            :pagination="pagination",
-            dense
-          )
-      q-markup-table(dense, cell)
-        thead
-          tr
-            th
-            th(v-for="head in stats.header") {{ head }}
-        tbody
-          tr(v-for="(tr, i) in stats.data")
-            td {{ i }}
-            td(v-for="td in tr") {{ td }}
-
-      .code.c_code(style="max-height: unset") {{ stats }}
+      div(v-else-if="selectedView.id === 2")
+        template(v-if="stats.surveys")
+          div(v-for="survey in stats.surveys")
+            q-table(
+              :columns="survey.questions",
+              :data="survey.awnsers",
+              :pagination="pagination",
+              dense
+            )
+      div(v-else-if="selectedView.id === 3")
+        q-markup-table(dense, cell)
+          thead
+            tr
+              th
+              th(v-for="head in stats.header") {{ head }}
+          tbody
+            tr(v-for="(tr, i) in stats.data")
+              td {{ i }}
+              td(v-for="td in tr") {{ td }}
+        q-btn(color="primary" icon-right="archive" label="Export to csv" no-caps @click="exportTable(stats)")
+      div(v-else-if="selectedView.id === 4")
+        .code.c_code(style="max-height: unset") {{ stats }}
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
+import { exportFile } from 'quasar'
+
 // import { mapGetters } from 'vuex'
+
+function wrapCsvValue (val, formatFn) {
+  let formatted = formatFn !== void 0
+    ? formatFn(val)
+    : val
+
+  formatted = formatted === void 0 || formatted === null
+    ? ''
+    : String(formatted)
+
+  formatted = formatted.split('"').join('"XXX"')
+  /**
+   * Excel accepts \n and \r in strings, but some other CSV parsers do not
+   * Uncomment the next two lines to escape new lines
+   */
+  // .split('\n').join('\\n')
+  // .split('\r').join('\\r')
+
+  return `"${formatted}"`
+}
 
 const constStatistics = [
   {
@@ -139,12 +189,48 @@ export default {
       // IDs
       stats: null,
 
-      //
+      // Selected View
       selectedStatistic: null,
       currentStatisticId: null,
+      selectedView: 1,
 
       // Statistic Options
       statistics: constStatistics,
+      views: [
+        {
+          id: 1,
+          label: "View 1",
+          value: "View 1",
+          description: "View 1",
+          icon: "insights",
+        },
+        {
+          id: 2,
+          label: "View 2",
+          value: "View 2",
+          description: "View 2",
+          icon: "insights",
+        },
+        {
+          id: 3,
+          label: "Markup-Table",
+          value: "View 3 - Markup Table",
+          description: "Markuptable!",
+          icon: "insights",
+        },
+        {
+          id: 4,
+          label: "Blank JSON",
+          value: "View 4",
+          description: "View 4 - Blank Json",
+          icon: "insights",
+        },
+      ],
+
+      // Filter Data
+      filterData: {
+        limit: null
+      },
 
       // Pagination
       pagination: {
@@ -230,6 +316,7 @@ export default {
         .post("/api/backend/surveys-statistics", {
           type: statisticId,
           ids: surveyIds,
+          filter: this.filterData
         })
         .then((res) => {
           this.stats = res.data;
@@ -278,6 +365,34 @@ export default {
     sameObjects(ob1, ob2) {
       return JSON.stringify(ob1) == JSON.stringify(ob2);
     },
+
+    // Export Table
+    exportTable (stats = this.stats) {
+
+      // naive encoding to csv format
+      const content = [ stats.header.map(col => wrapCsvValue(col)) ].concat(
+        stats.data.map(row => stats.header.map(col => wrapCsvValue(
+          typeof col.field === 'function'
+            ? col.field(row)
+            : row[col === void 0 ? col : col],
+          col.format
+        )).join(';'))
+      ).join('\r\n')
+
+      const status = exportFile(
+        'table-export.csv',
+        content,
+        'text/csv'
+      )
+
+      if (status !== true) {
+        this.$q.notify({
+          message: 'Browser denied file download...',
+          color: 'negative',
+          icon: 'warning'
+        })
+      }
+    }
   },
 };
 </script>
