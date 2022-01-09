72c34bd324e5066e19453e8e254f1bea55e6a953
Your Name <you@example.com>
2022-01-09 19:57:39 +0100

    quickfix

--- src/components/Backend/Statistics/Types/Chart.vue
+++ src/components/Backend/Statistics/Types/Chart.vue
@@ -2,44 +2,79 @@
 .chart(v-if="loaded")
   h1 {{ question_title }}
 
-  keep-alive(
-    v-if="enable"
-    v-for="type in chartTypes"
-  )
+  keep-alive(v-if="enable", v-for="type in chartTypes")
     component(
-      :is="type"
-      :stats="stats"
-      :question_title="question_title"
-      :filteredCount="filteredCount"
-      :chartData="chartData"
+      :is="type",
+      :stats="stats",
+      :question_title="question_title",
+      :filteredCount="filteredCount",
+      :chartData="chartData",
       :options="options"
     )
-
 </template>
 
 <script>
 export default {
-
   props: ["stats", "question", "question_id"],
 
   data() {
     return {
-      // Disable first, then loaded
+      // Disable it first, then set it to loaded
       loaded: false,
 
       // Chart Types
       chartTypes: [
-        () => import('@/components/Backend/Statistics/Types/Charts/PieChart.vue'),
-        () => import('@/components/Backend/Statistics/Types/Charts/BarChart.vue'),
+        () =>
+          import("@/components/Backend/Statistics/Types/Charts/PieChart.vue"),
+        () =>
+          import("@/components/Backend/Statistics/Types/Charts/BarChart.vue"),
       ],
 
-      // Data
+      // Filtering Data
       filteredCount: {},
-      // commentsCount: {},
+    };
+  },
 
-      // Charts
-      chartData: {},
-      options: {
+  computed: {
+    filteredValues () {
+      return this.filterNulls(Object.values(this.filteredCount));
+    },
+
+    filteredKeys () {
+      return this.filterNulls(Object.keys(this.filteredCount));
+    },
+
+    filteredKeysColor () {
+      return this.filteredKeys.map(
+        (e) => this.stats.find((x) => x.option_title === e)?.option_color
+      );
+    },
+
+    question_title () {
+      return this.question?.question_title;
+    },
+
+    enable () {
+      return !["info_only", "comment_only"].includes(
+        this.question?.question_format
+      );
+    },
+
+    chartData () {
+      return {
+        datasets: [
+          {
+            label: this.question_title,
+            backgroundColor: this.filteredKeysColor,
+            data: this.filteredValues,
+          },
+        ],
+        labels: this.filteredKeys,
+      };
+    },
+
+    options () {
+      return {
         scales: {
           yAxes: [
             {
@@ -64,31 +99,7 @@ export default {
         },
         responsive: true,
         maintainAspectRatio: false,
-      },
-    };
-  },
-
-  computed: {
-    filteredValues() {
-      return this.filterNulls(Object.values(this.filteredCount));
-    },
-
-    filteredKeys() {
-      return this.filterNulls(Object.keys(this.filteredCount));
-    },
-
-    filteredKeysColor() {
-      return this.filteredKeys.map(
-        (e) => this.stats.find((x) => x.option_title === e)?.option_color
-      );
-    },
-
-    question_title() {
-      return this.question?.question_title;
-    },
-
-    enable () {
-      return !["info_only", 'comment_only'].includes(this.question?.question_format)
+      }
     },
   },
 
@@ -101,22 +112,11 @@ export default {
 
     // this.stats.map(e=>e.awnser_comment).forEach(x => this.commentsCount[x] = (this.commentsCount[x] || 0)+1)
 
-    this.chartData = {
-      datasets: [
-        {
-          label: this.question_title,
-          backgroundColor: this.filteredKeysColor,
-          data: this.filteredValues,
-        },
-      ],
-      labels: this.filteredKeys,
-    };
-
     // console.log(
     //   this.question_title, this.countKeys, this.countValues
     // )
 
-    this.loaded = true
+    this.loaded = true;
   },
 
   methods: {
